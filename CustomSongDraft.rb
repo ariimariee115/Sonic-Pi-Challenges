772eeb6fd2b758c4844aaf56fb434ef18eea2dc5
ariimariee115 <auria_moore@student.davincischools.org>
2026-03-02 11:24:52 -0800

    Create CustomSongDraft.rb

---
+++ CustomSongDraft.rb
@@ -0,0 +1,34 @@
+#Passionfruit by Drake
+"C:/Users/auria_moore/Documents/Audacity/Passionfruit.wav"
+
+
+use_bpm 113
+
+live_loop:high_notes do
+  play :fs5
+  sleep 1
+  play :cs5
+  sleep 5
+  play :b4
+  sleep 2
+  play :fs5
+  sleep 1
+  play :as4
+  sleep 4
+  play :as4
+  sleep 1
+  play :gs4
+  sleep 2
+  
+end
+
+live_loop:low_notes do
+  play :es3
+  sleep 0.75
+  play :b4
+  sleep 0.75
+  play :cs4
+  sleep 0.5
+  
+end
+
