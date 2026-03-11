af1e5b4bdffa9754972694896513e1be15b54e23
ariimariee115 <auria_moore@student.davincischools.org>
2026-03-11 10:39:04 -0700

    Create challenge_DDD.rb

---
+++ challenge_DDD.rb
@@ -0,0 +1,126 @@
+# CHALLENGE D: Use functions to optimize the SpongeBob theme!
+
+use_bpm 150
+use_synth :piano
+
+live_loop :background_notes do
+  play:E3, amp: 0.25
+  sleep 1
+  
+  play:B3, amp: 0.25
+  play:E4, amp: 0.25
+  sleep 1
+  play:B3, amp: 0.25
+  sleep 1
+  play:E4, amp: 0.25
+  sleep 1
+  
+  play:E3, amp: 0.5
+  sleep 1
+  play:B3, amp: 0.5
+  play:E4, amp: 0.5
+  sleep 1
+  play:B3, amp: 0.5
+  sleep 1
+  play:E4, amp: 0.5
+  sleep 1
+  
+  play:E3, amp: 0.75
+  sleep 1
+  play:B3, amp: 0.75
+  play:E4, amp: 0.75
+  sleep 1
+  play:B3, amp: 0.75
+  sleep 1
+  play:E4, amp: 0.75
+  sleep 1
+  
+  5.times do
+    play:E3, amp: 1
+    sleep 1
+    play:B3, amp: 1
+    play:E4, amp: 1
+    sleep 1
+    play:B3, amp: 1
+    sleep 1
+    play:E4, amp: 1
+    sleep 1
+  end
+  stop
+end
+
+# Measure 1
+define :measure_one do
+  play :r
+  sleep 1
+  play:E4
+  play:Gs4
+  sleep 2
+  play:E5
+  play:Gs4
+  sleep 1
+end
+# Measure 2
+define :pink do
+  play:E4
+  sleep 1
+  play:E5
+  play:Gs4
+  sleep 2
+  play:E5
+  play:Gs4
+  sleep 1
+end
+# Measure 3
+define :yellow do
+  play :r
+  sleep 1
+  play:E5
+  play:Gs4
+  play:B4
+  sleep 1
+  play:B4
+  sleep 1
+  play:E5
+  play:Gs4
+  sleep 1
+end
+
+# Measure 4
+yellow
+
+
+
+# Measure 5
+define :blue do
+  play:B4
+  sleep 0.75
+  play:As4
+  sleep 0.25
+  play:Gs4
+  play:B4
+  sleep 0.75
+  play:Cs5
+  sleep 0.25
+  play:B4
+  sleep 1
+  play:Gs4
+  play:E5
+  sleep 1
+end
+
+# Measure 6
+yellow
+
+
+# Measure 7
+blue
+
+
+# Measure 8
+pink
+
+
+with_fx :ping_pong do
+  measure_one
+end
