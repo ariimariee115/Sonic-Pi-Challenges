c758c1eba1a794abba6710c0922e18d3267f1ff8
ariimariee115 <auria_moore@student.davincischools.org>
2026-03-12 10:17:53 -0700

    Create arrays_lyrics.rb

---
+++ arrays_lyrics.rb
@@ -0,0 +1,11 @@
+#Question: Why do we need to use an array?
+
+lyrics = ["come along","with","me","to a town","beside the sea","we can wander","thru the forest","and do so"]
+i = 0
+
+
+9.times do
+  print(lyrics[i])
+  sleep 0.5
+  i = i + 1
+end
