f7f3583de40b0c6bcf179890f095f4d9c44f13b1
ariimariee115 <auria_moore@student.davincischools.org>
2026-03-26 11:07:40 -0700

    Create studentQuiz.rb

---
+++ studentQuiz.rb
@@ -0,0 +1,19 @@
+
+# the function below accepts no parameter (inputs)
+define :studentQuizScore do |name, score|
+  print("STUDENT NAME: ")
+  print (name)
+  print("QUIZ SCORE: ")
+  print(score)
+end
+
+#call the function
+studentQuizScore "Eric" , 2
+
+studentQuizScore "Ty" , 3
+
+studentQuizScore "Angela" , 4
+
+studentQuizScore "Sandy" , 3.5
+
+studentQuizScore "Quinn" , 1.5
